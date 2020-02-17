36462cc4170a09be8a6f33123a37ed6dfed37ce5
Lesly84 <llopez84@ucmerced.edu>
2020-02-17 14:04:52 -0800

    add binomial and poisson distributions

--- Code/db_model.py
+++ Code/db_model.py
@@ -7,31 +7,60 @@ import argparse
 import db_model_functions as db
 
 parser = argparse.ArgumentParser(description="Run distance-based model.")
+subparsers=parser.add_subparsers(help="sub-command help")
 
-parser.add_argument('-alpha',action="store",dest="alpha",type=int,help="Alpha is strength of isolation.")
-parser.add_argument('-type',action="store",dest="type",help="Probability of choosing parent: 'linear' or 'exponential'.")
-parser.add_argument('-n',action="store",dest="N",type=int,help="Constant population size.")
-parser.add_argument('--standard',action = "store_true",default=False,help="Run standard Wright Fisher model.")
+parser.add_argument('-N',action="store",dest="N",type=int,help="Constant population size.")
 parser.add_argument('-r',action="store",dest="run",help="What run number is this?")
-#type = "exponential" #"linear" # or "exponential"
+parser.add_argument('--db_model',action="store_true",default=False,help="Run db-model.")
+parser.add_argument('--binomial',action="store_true",default=False,help="Run model with binomial distribution.")
+parser.add_argument('--poisson',action="store_true",default=False,help="Run model with poisson distribution.")
+#per generation outfile
+parser.add_argument('--per_generation',action="store_true",default=False,help="Output file with number of individuals per generation.")
+
+#Create parser for binomial distribution
+parser_bi = subparsers.add_parser('bi',help='binomial distribution help')
+#parser_bi.add_argument('-n',type=int,action="store",dest="binom_n",help="Size/Number of trials (n) for binomial distribution.")
+#parser_bi.add_argument('-p',type=float,action="store",dest="binom_p",help="Probability of a success for binomial distribution.")
+parser_bi.add_argument('--binomial',action = "store_true", default=False,help="Use binomial distribution to pick the parent's location.")
+parser_bi.set_defaults(binomial=True)
+
+#Creat subparser for poisson distribution
+parser_poisson = subparsers.add_parser('poisson',help="Poisson distribution help.")
+#parser_poisson.add_argument('-mu',type=int,action="store",dest="poisson_mu",help="Mu for poisson distribution.")
+parser_poisson.set_defaults(poisson=True)
+
+#Creat subparser for linear db-model
+parser_db = subparsers.add_parser('db',help="Linear and exponential db-model help.")
+parser_db.add_argument('-alpha',action="store",dest="alpha",type=int,help="Alpha is strength of isolation.")
+parser_db.add_argument('-type',action="store",dest="type",choices=['linear-db','exponential-db'],help="Probability of choosing parent: 'linear-db' or 'exponential-db'.")
+parser_db.add_argument('--standard',action = "store_true",default=False,help="Run standard Wright Fisher model with the db-model.")
+parser_db.set_defaults(db_model=True)
+
 param = parser.parse_args()
 N = param.N
 
-if(param.standard):
-  if(param.type=="linear"):
-    param.alpha = 0
-  elif(param.type=="exponential"):
-    param.alpha = 1
-  else:
-    print("Specify model type 'linear' or 'exponential'")
-    sys.exit()
+#if(param.standard):
+#  if(param.type=="linear"):
+#    param.alpha = 0
+#  elif(param.type=="exponential"):
+#    param.alpha = 1
+#  else:
+#    print("Specify model type 'linear' or 'exponential'")
+#    sys.exit()
 
 #Outfile
-outfile_path = "/home/llopez84/qsb/model-collab/Data/"
-outfile = outfile_path +param.type+"_db-model_"+str(N)+"-N_"+str(param.alpha)+"-alpha.csv"
-per_generation_outfile = outfile_path + param.type+"_db-model_"+str(N)+"-N_"
-per_generation_outfile += str(param.alpha)+"-alpha_ind-per-generation.csv"
-ind_name_outfile = outfile_path + "positions_individuals_mrca_"+param.type+"_db-model_"+str(param.alpha)+"-alpha_"+str(N)+"-N.csv"
+outfile_path = "../Results/"
+if(param.db_model):
+  outfile = outfile_path +param.type+"_db-model_"+str(N)+"-N_"+str(param.alpha)+"-alpha.csv"
+elif(param.binomial):
+  outfile = outfile_path + "binomial_distribution_db-model_"+str(N)+"-N.csv"
+elif(param.poisson):
+  outfile = outfile_path + "poisson_distribution_db-model_"+str(N)+"-N.csv"
+
+#per generation
+if(param.per_generation):
+  per_generation_outfile = outfile_path + param.type+"_db-model_"+str(N)+"-N_"
+  per_generation_outfile += str(param.alpha)+"-alpha_ind-per-generation.csv"
 
 t = 0
 parents = {}
@@ -42,22 +71,16 @@ mrca = individuals_list[t]
 #Keep track of the times a coalescent event occurs
 coalescent_time = []
 
-#Get position of all starting individuals
-pos = range(1,N+1)
-#Get dij matrix: distance between individuals i and individuals j
-dij = db.get_dij_matrix(N=N,pos=pos)
-#Get probability matrix for an individual in each position to get a parent based on position
-pij = db.probability_picking_parent(param.alpha,dij,N,param.type)
+##For linear and exponential db-model
+if(param.db_model):
+  #Get position of all starting individuals
+  pos = range(1,N+1)
+  #Get dij matrix: distance between individuals i and individuals j
+  dij = db.get_dij_matrix(N=N,pos=pos)
+  #Get probability matrix for an individual in each position to get a parent based on position
+  pij = db.probability_picking_parent(param.alpha,dij,N,param.type)
 
 data=[]
-
-#Open individuals outfile
-if(os.path.exists(ind_name_outfile)):
-  individuals_file = open(ind_name_outfile,"a+")
-else:
-  individuals_file = open(ind_name_outfile,"a+")
-  header = "model\tN\talpha\ttime\tsample\tindividual\tindividual_position\n"
-  individuals_file.write(header)
 
 while(len(mrca)!= 1 ):
   #Get individuals we are finding parents for in the time generation t
@@ -67,7 +90,15 @@ while(len(mrca)!= 1 ):
   individuals_list.append(possible_parents)
   for i in range(N):
     ind = individuals[i]
-    parent_index = np.random.choice(N,p=pij[i])
+    #For binomial distribution: need sample size (N) and location of the offspring (index i)
+    if(param.binomial):
+      parent_index = db.choose_parent_location_binomial(N,i)
+    #For poisson distribution: need sample size (N) and location of the offspring (index i)
+    if(param.poisson):
+      parent_index = db.choose_parent_location_poisson(N,i)
+    #For db-model
+    if(param.db_model):
+      parent_index = np.random.choice(N,p=pij[i])
     parent = possible_parents[parent_index]
     parents[ind] = parent
   allmrca = [parents[m] for m in mrca]
@@ -76,42 +107,37 @@ while(len(mrca)!= 1 ):
     coalescent_time.append(t)
   t+= 1
   data.append((param.run,t,len(mrca)))
-#  #individuals_outfile info
-#  if(len(mrca)<=3):
-#    for mrca_ind in mrca:
-#      individuals_outline = param.type+"\t"+str(param.N)+"\t"+str(param.alpha)+"\t"+str(t)+"\t"+str(len(mrca))+"\t"+str(mrca_ind)
-#      individuals_outline += "\t"+str(mrca_ind%param.N)+"\n"
-#      individuals_file.write(individuals_outline)
 
-#Close individuals outfile
-individuals_outline = param.type+"\t"+str(param.N)+"\t"+str(param.alpha)+"\t"+str(t)+"\t"+str(len(mrca))+"\t"+str(mrca[0])
-individuals_outline += "\t"+str(mrca[0]%param.N)+"\n"
-individuals_file.write(individuals_outline)
+#Output tmrca or t
+#db-model
+if(param.db_model):
+  header = "model\truns\tN\talpha\ttmrca\n"
+  outline = param.type+"\t"+str(param.run)+"\t"+str(N)+"\t"+str(param.alpha)+"\t"+str(t)
+elif(param.binomial):
+  header = "model\t\runs\t\N\tmrca\n"
+  outline = "binomial\t"+str(param.run)+"\t"+str(N)+"\t"+str(t)
+elif(param.poisson):
+  header  = "model\t\runs\t\N\tmrca\n"
+  outline = "poisson\t"+str(param.run)+"\t"+str(N)+"\t"+str(t)
 
-individuals_file.close()
+if(os.path.exists(per_generation_outfile)):
+  fout = open(outfile,"a+")
+  fout.write(outline+"\n")
+  fout.close()
+else:
+  fout = open(outfile,"w")
+  fout.write(header)
+  fout.write(outline+"\n")
+  fout.close()
 
-
-##Output tmrca or t
-#if(os.path.exists(per_generation_outfile)):
-#  fout = open(outfile,"a+")
-#  outline = param.type+"\t"+str(param.run)+"\t"+str(N)+"\t"+str(param.alpha)+"\t"+str(t)
-#  fout.write(outline+"\n")
-#  fout.close()
-#else:
-#  fout = open(outfile,"w")
-#  header = "model\truns\tN\talpha\ttmrca\n"
-#  fout.write(header)
-#  outline = param.type+"\t"+str(param.run)+"\t"+str(N)+"\t"+str(param.alpha)+"\t"+str(t)
-#  fout.write(outline+"\n")
-#  fout.close()
-
-##second outfile to store both number of individuals and time in generations
-#columns=["run","time","number of individuals"]
-#df = pd.DataFrame(data,columns=columns)
-#if(os.path.exists(per_generation_outfile)):
-#  df.to_csv(per_generation_outfile,index=False,sep="\t",mode="a",header=False)
-#else:
-#  df.to_csv(per_generation_outfile,index=False,sep="\t")
+#second outfile to store both number of individuals and time in generations
+if(param.per_generation):
+  columns=["run","time","number of individuals"]
+  df = pd.DataFrame(data,columns=columns)
+  if(os.path.exists(per_generation_outfile)):
+    df.to_csv(per_generation_outfile,index=False,sep="\t",mode="a",header=False)
+  else:
+    df.to_csv(per_generation_outfile,index=False,sep="\t")
 
 ##To go through every individual one by one
 #range(t*N)
--- Code/db_model_functions.py
+++ Code/db_model_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.stats
 import sys
 
 def get_dij_matrix(pos,N):
@@ -19,7 +20,38 @@ def linear_prob_numerator(dsum,dij_number,alpha):
 def exponential_prob_numerator(dij_number,alpha):
   return(alpha**(-dij_number))
 
-def probability_picking_parent(alpha,dij,N,type):
+def binomial_distribution(n,p):
+  #Binomial distribution can be used without modification. Get a random number from bi-dist
+  return(scipy.stats.binom.rvs(n, p, size=1)[0])
+
+def choose_parent_location_binomial(N,offspring_location):
+  n = N - 1
+  p = offspring_location/float(n)
+  return(scipy.stats.binom.rvs(n,p,size=1)[0])
+
+def choose_parent_location_poisson(N,offspring_location):
+  #If offspring's location is smaller than the middle location
+  if(offspring_location < (N/2)):
+    #use regular poisson distribution
+    location = scipy.stats.poisson.rvs(offspring_location, size=1)[0]
+    while(location > N - 1):
+      location = scipy.stats.poisson.rvs(offspring_location, size=1)[0]
+  else:
+    #If sample size (N) is even
+    if(N%2==0):
+      #Use the left side of the poisson distribution
+      #To reflect: N - location of parent from poisson distribution
+      adjusted_mu = N - offspring_location - 1
+      location = N - scipy.stats.poisson.rvs(adjusted_mu,size=1)[0]
+    #If sample size (N) is odd
+    else:
+      #Use the left side of the poisson distribution
+      #To reflect: N - location of parent from poisson distribution
+      adjusted_mu = N - offspring_location - 1
+      location = N - scipy.stats.poisson.rvs(adjusted_mu,size=1)[0] - 1
+  return(location)
+
+def probability_picking_parent(alpha,dij,N,model_type):
   #Check that probability add to zero
   error = 1e-10
   assert(np.shape(dij)[0]==N)
@@ -29,12 +61,16 @@ def probability_picking_parent(alpha,dij,N,type):
     #Sum of all distances from individual i to all other individuals (j)
     dsum = sum(dij[i])
     for j in range(N):
-      if(type=="linear"):
+      if(model_type=="linear-db"):
         pij[i][j] = linear_prob_numerator(dsum,dij[i][j],alpha)
-      elif(type=="exponential"):
+      elif(model_type=="exponential-db"):
         pij[i][j] = exponential_prob_numerator(dij[i][j],alpha)
+#      elif(model_type=="binomial"):
+#        pij[i][j] = 
+#      elif(model_type=="exponential"):
+#        pij[i][j] = 
       else:
-        print("specify probability type 'linear' or 'exponential'")
+        print("specify probability type 'linear-db','exponential-db','binomial' or 'exponential'")
         sys.exit()
       denominator += pij[i][j]
     pij[i] /= denominator
